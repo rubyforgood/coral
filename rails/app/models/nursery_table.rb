9b3d5fbe2d4df910be70b322d2d8004e8f1dec44
alitopaloglu <ali_t@gwmail.gwu.edu>
2019-07-26 17:24:15 -0400

    Add validation for positve capacity of nursery tables

--- rails/app/models/nursery_table.rb
+++ rails/app/models/nursery_table.rb
@@ -1,3 +1,4 @@
 class NurseryTable < ApplicationRecord
   belongs_to :zone
+  validates :capacity, numericality: { greater_than_or_equal_to: 1 }
 end
--- rails/spec/models/nursery_table_spec.rb
+++ rails/spec/models/nursery_table_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe NurseryTable, type: :model do
   it "has valid attributes" do
     expect(nursery).to have_attributes(capacity: 200, name: "A", zone_id: zone.id)
   end
+
+  it "cannot have negative capacity" do
+    nursery = NurseryTable.new(capacity: -20 ,name: "A", zone_id: zone.id)
+    expect(nursery).to_not be_valid
+  end
+
+  it "cannot be zero" do
+    zone = Zone.create(id: 13, name:"Add me")
+    nursery = NurseryTable.new(capacity: 0 ,name: "A", zone_id: zone.id)
+    expect(nursery).to_not be_valid
+  end
+
+  it "atleast has one" do
+    zone = Zone.create(id: 23, name:"Add me")
+    nursery = NurseryTable.new(capacity: 1 ,name: "A", zone_id: zone.id)
+    expect(nursery).to be_valid
+  end
 end
