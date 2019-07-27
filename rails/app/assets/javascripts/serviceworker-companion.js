a8e8423deac2fc661d3de7738436d6f076a2c857
Betsy Haibel <betsy.haibel@gmail.com>
2019-07-27 10:01:30 -0400

    service worker can recieve messages

--- rails/app/assets/javascripts/serviceworker-companion.js
+++ rails/app/assets/javascripts/serviceworker-companion.js
@@ -1,6 +1,8 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
     .then(function(reg) {
+      var registrationEvent = new Event('serviceWorkerRegistered');
+      document.dispatchEvent(registrationEvent);
       console.log('[Companion]', 'Service worker registered!');
     });
 }
