1bb417a4d7e9604cad644daf14232425d7762c25
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-27 12:05:09 -0400

    Reorder user routes

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :restoration_activity_log_entries
   root to: 'home#index'
-  devise_for :users  
+  resources :users, only: [:index, :show]
+  devise_for :users
   resources :nursery_tables
   resources :zones
   resources :dives, only: [:new, :create, :show] do
@@ -12,7 +13,7 @@ Rails.application.routes.draw do
     get 'sign_in', to: 'devise/sessions#new'
     get 'sign_up', to: 'devise/registrations#new'
   end
-  resources :users, only: [:index, :show]
+  
 
   root to: 'home#index'
 end
