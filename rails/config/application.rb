213b444970991f3f35d58e88989346c8eaeb06b9
Yungchih Chen <yungchih_chen@yahoo.com>
2019-07-27 21:19:29 -0400

    Add internationalize I18n

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -29,5 +29,11 @@ module Coral
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :fr]
   end
 end
