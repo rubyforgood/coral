347ffb2f6a63e713d2cdb64fba8b0153823829fe
Conor Sturgess <conhub93@gmail.com>
2019-08-13 23:01:25 +0100

    Renamed column :broken_coral on restoration_activity_log_entries to :bleached_coral

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_201251) do
-
+ActiveRecord::Schema.define(version: 20_190_727_153_915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_08_13_201251) do
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+    t.index %w[record_type record_id name blob_id], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
