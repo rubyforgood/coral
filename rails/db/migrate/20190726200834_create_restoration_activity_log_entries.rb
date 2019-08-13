fcd9775278f07f28a0023f050fc63579860a69eb
Conor Sturgess <conhub93@gmail.com>
2019-08-13 22:25:45 +0100

    Renamed column :broken_coral on restoration_activity_log_entries to :bleached_coral

--- rails/db/migrate/20190726200834_create_restoration_activity_log_entries.rb
+++ rails/db/migrate/20190726200834_create_restoration_activity_log_entries.rb
@@ -3,7 +3,7 @@ class CreateRestorationActivityLogEntries < ActiveRecord::Migration[5.2]
     create_table :restoration_activity_log_entries do |t|
       t.boolean :cleaned
       t.integer :percent_filled
-      t.integer :bleached_corals
+      t.integer :broken_columns
       t.integer :dead_corals
       t.integer :dive_id
       t.integer :nursery_table_id
