fb369c14a00fabeb86df8991bb21ee3ac6c14968
Ryan B. Harvey <ryan.b.harvey@ted.com>
2019-08-17 02:15:37 -0500

    Implement internationalization based on URL path not query string

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -2,18 +2,22 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  before_action :set_locale
+  around_action :set_locale
 
-  def set_locale
-    I18n.locale = I18n.default_locale
+  protected
+  
+  def set_locale(&action)
     locale = params[:locale].to_s.strip.to_sym
-
-    I18n.locale = locale if I18n.available_locales.include?(locale)
+    if (!I18n.available_locales.include?(locale))
+      locale = I18n.default_locale
+    end
+    I18n.with_locale(locale, &action)
   end
 
-  # def default_url_options(options = {})
-  #   options.merge(locale: I18n.locale)
-  # end
+  def default_url_options(options = {})
+    # options.merge(locale: I18n.locale)
+    { locale: I18n.locale == I18n.default_locale ? I18n.default_locale : I18n.locale }
+  end
 
   protected
 
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,30 +1,34 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  get "/*path", to: redirect("/#{I18n.default_locale}/%{path}"),
+    constraints: {
+      path: %r{(?!(#{I18n.available_locales.join('|')})).*}
+    }
+    # I18n.available_locales.map(&:to_s).includes?(path.split('/').first)
 
-  # User routes
-  devise_for :users
-  resources :users, only: %i[index show]
-  devise_scope :user do
-    get "sign_in", to: "devise/sessions#new"
-    get "sign_up", to: "devise/registrations#new"
-  end
-
-  resources :restoration_activity_log_entries
-  resources :nursery_tables
-  resources :zones
-  resources :dives, only: %i[new create show] do
-    post :finish, on: :member
-    resources :restoration_activity_log_entries, only: %i[new create]
-  end
-
-  devise_scope :user do
-    get "sign_in", to: "devise/sessions#new"
-    get "sign_up", to: "devise/registrations#new"
-  end
-
-  resources :users, only: %i[index show]
-
-  scope "(:locale)", locale: /en|fr/ do
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
     root to: "home#index"
+
+    # User routes
+    devise_for :users
+    resources :users, only: %i[index show]
+    devise_scope :user do
+      get "sign_in", to: "devise/sessions#new"
+      get "sign_up", to: "devise/registrations#new"
+    end
+
+    resources :restoration_activity_log_entries
+    resources :nursery_tables
+    resources :zones
+    resources :dives, only: %i[new create show] do
+      post :finish, on: :member
+      resources :restoration_activity_log_entries, only: %i[new create]
+    end
+
+    devise_scope :user do
+      get "sign_in", to: "devise/sessions#new"
+      get "sign_up", to: "devise/registrations#new"
+    end
+
+    resources :users, only: %i[index show]
   end
 end
