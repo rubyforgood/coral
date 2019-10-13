c32a58ef98bd487524ef0c620262a279851064b6
laser-shark <amazing.laser.sharks@gmail.com>
2019-10-13 12:17:27 +0200

    Add test for delete route

--- rails/spec/requests/restoration_activity_logs_spec.rb
+++ rails/spec/requests/restoration_activity_logs_spec.rb
@@ -111,5 +111,38 @@ RSpec.describe "Restoration Activity Log", type: :request do
         end
       end
     end
+
+    describe "DELETE" do
+      context "given images" do
+        subject do
+          lambda {
+            delete "/restoration_activity_log_entries/#{log_entry.id}", params: {
+              restoration_activity_log_entry: {
+                images: images,
+                nursery_table_id: nursery_table.id
+              }
+            }
+          }
+        end
+
+        let(:log_entry) { FactoryBot.create(:restoration_activity_log_entry) }
+
+        let(:images) do
+          [
+            fixture_file_upload(Rails.root.join("spec", "factories", "images", "GOPR3892.JPG")),
+            fixture_file_upload(Rails.root.join("spec", "factories", "images", "GOPR3893.JPG"))
+          ]
+        end
+
+        it "redirects" do
+          subject.call
+          expect(response).to be_redirect
+
+          follow_redirect!
+          expect(response).to be_successful
+          expect(response.body).to include("Restoration activity log entry was successfully destroyed.")
+        end
+      end
+    end
   end
 end
